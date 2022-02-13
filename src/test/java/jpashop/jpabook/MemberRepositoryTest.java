31438debc610b55f8f1e780e22955cad054fed68
kylle <kylle@naver.com>
2022-02-14 08:10:36 +0900

    jpabook 프로젝트 생성

---
+++ src/test/java/jpashop/jpabook/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package jpashop.jpabook;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import javax.transaction.Transactional;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional()
+    public void save() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("안중근");
+
+        //when
+        Long id = memberRepository.sava(member);
+        Member findMember = memberRepository.find(id);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+
+
+}
