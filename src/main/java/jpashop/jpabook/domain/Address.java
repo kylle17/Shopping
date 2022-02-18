e381888c50e57405f0f69f907686d4732491b9e1
kylle <kylle@naver.com>
2022-02-18 23:09:38 +0900

    엔티티 매핑4

--- src/main/java/jpashop/jpabook/domain/Address.java
+++ src/main/java/jpashop/jpabook/domain/Address.java
@@ -1,12 +1,25 @@
 package jpashop.jpabook.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Embeddable
+@Getter
 public class Address {
+
     String city;
     String street;
     String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpashop/jpabook/domain/Category.java
+++ src/main/java/jpashop/jpabook/domain/Category.java
@@ -12,14 +12,24 @@ import java.util.List;
 @Getter @Setter
 public class Category {
 
-    @Id @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue
+    @Column(name = "category_id")
     private Long id;
 
     private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
     private Category parent;
 
-    private List<Category> child;
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
 
 }
