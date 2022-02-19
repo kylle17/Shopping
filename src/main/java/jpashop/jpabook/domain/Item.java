4c5318ef6f2758ca5c4afd9363bb8dc28fb3c2fd
kylle <kylle@naver.com>
2022-02-19 22:06:38 +0900

    엔티티 매핑 5

--- src/main/java/jpashop/jpabook/domain/Item.java
+++ src/main/java/jpashop/jpabook/domain/Item.java
@@ -5,25 +5,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "item")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Getter @Setter
-public class Item {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+public abstract class Item {
+    @Id @GeneratedValue
     @Column(name = "item_id")
     private Long id;
-
     private String name;
     private int price;
     private int stockQuantity;
 
-
-    private List<Category> categories;
-
-
-
+    @ManyToMany(mappedBy = "items" , fetch = FetchType.LAZY )
+    private List<Category> categories = new ArrayList<Category>();
 }
